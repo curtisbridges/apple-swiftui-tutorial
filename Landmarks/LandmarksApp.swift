b47d2ff2983feb4ca4c0df413642d9c5b167057d
Curtis Bridges <curtis@curtisbridges.com>
2020-11-15 08:44:25 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Curtis Bridges on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
